71bfd4e684c0625bf70277a5878e9e3290d24d33
takashi11171117 <takashi11171117@gmail.com>
2024-03-11 01:50:40 +0900

    reload時に404になる問題の対応

--- resources/ts/AppRouter.tsx
+++ resources/ts/AppRouter.tsx
@@ -4,6 +4,7 @@ import { Page1 } from '@/pages/Page1'
 import { Page2 } from '@/pages/Page2'
 import { Routes, Route } from 'react-router-dom'
 import { Login } from '@/pages/auth/Login'
+import { Page404 } from '@/pages/Page404'
 
 const AppRouter: React.FC = () => {
   return (
@@ -12,6 +13,7 @@ const AppRouter: React.FC = () => {
       <Route path="/page1" element={<Page1 />} />
       <Route path="/page2" element={<Page2 />} />
       <Route path="/auth/login" element={<Login />} />
+      <Route path="*" element={<Page404 />} />
     </Routes>
   )
 }
