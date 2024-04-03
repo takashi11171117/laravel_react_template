c82d096bf52a8f39aa139a178b4db75a5f0b52fa
takashi11171117 <takashi11171117@gmail.com>
2024-04-03 20:18:13 +0900

    修正

--- resources/ts/features/sample/hooks/api/todos/hooks.ts
+++ resources/ts/features/sample/hooks/api/todos/hooks.ts
@@ -1,60 +1,39 @@
-import { AxiosError } from 'axios'
-import {
-  useQuery,
-  useMutation,
-  UseQueryOptions,
-  UseMutationOptions,
-} from '@tanstack/react-query'
-import {
-  UpdateTodoDTO,
-  updateTodo,
-} from '@/features/sample/hooks/api/todos/updateTodo'
-import {
-  StoreImageForTodoDTO,
-  storeImageForTodo,
-} from '@/features/sample/hooks/api/todos/storeImageForTodo'
-import {
-  UpdateImageForTodoDTO,
-  updateImageForTodo,
-} from '@/features/sample/hooks/api/todos/updateImageForTodo'
-import {
-  DeleteImageForTodDTO,
-  deleteImageForTodo,
-} from '@/features/sample/hooks/api/todos/deleteImageForTodo'
-import {
-  StorePDFForTodoDTO,
-  storePDFForTodo,
-} from '@/features/sample/hooks/api/todos/storePDFForTodo'
+import { useQuery, useMutation } from '@tanstack/react-query'
+import { updateTodo } from '@/features/sample/hooks/api/todos/updateTodo'
+import { storeImageForTodo } from '@/features/sample/hooks/api/todos/storeImageForTodo'
+import { updateImageForTodo } from '@/features/sample/hooks/api/todos/updateImageForTodo'
+import { deleteImageForTodo } from '@/features/sample/hooks/api/todos/deleteImageForTodo'
+import { storePDFForTodo } from '@/features/sample/hooks/api/todos/storePDFForTodo'
 import { fetchTodos } from '@/features/sample/hooks/api/todos/fetchTodos'
 import {
   FetchTodoDTO,
   fetchTodo,
 } from '@/features/sample/hooks/api/todos/fetchTodo'
-import {
-  CreateTodoDTO,
-  createTodo,
-} from '@/features/sample/hooks/api/todos/createTodo'
-import {
-  DeleteTodoDTO,
-  deleteTodo,
-} from '@/features/sample/hooks/api/todos/deleteTodo'
+import { createTodo } from '@/features/sample/hooks/api/todos/createTodo'
+import { deleteTodo } from '@/features/sample/hooks/api/todos/deleteTodo'
 import { todosKeys } from '@/features/sample/hooks/api/todos/todosKeys'
+import {
+  ExtractFnReturnType,
+  MutationConfig,
+  QueryConfig,
+} from '@/lib/reactQuery'
 
-export const useTodosQuery = <TData = Todo[]>(
-  options?: Omit<
-    UseQueryOptions<Todo[], AxiosError, TData, typeof todosKeys.all>,
-    'queryKey' | 'queryFn'
-  >,
-) => {
-  return useQuery({ queryKey: todosKeys.all, queryFn: fetchTodos, ...options })
+type FetchTodosFnType = typeof fetchTodos
+type FetchTodosOptions = {
+  config?: QueryConfig<FetchTodosFnType>
+}
+export const useTodosQuery = (options?: FetchTodosOptions) => {
+  return useQuery<ExtractFnReturnType<FetchTodosFnType>>({
+    queryKey: todosKeys.all,
+    queryFn: fetchTodos,
+    ...options,
+  })
 }
 
-export const useCreateTodoMutation = (
-  options?: Omit<
-    UseMutationOptions<void, AxiosError, CreateTodoDTO, typeof todosKeys.all>,
-    'mutationKey' | 'mutationFn'
-  >,
-) => {
+type CreateTodoOptions = {
+  config?: MutationConfig<typeof createTodo>
+}
+export const useCreateTodoMutation = (options?: CreateTodoOptions) => {
   return useMutation({
     mutationFn: createTodo,
     mutationKey: todosKeys.all,
@@ -62,12 +41,10 @@ export const useCreateTodoMutation = (
   })
 }
 
-export const useDeleteTodoMutation = (
-  options?: Omit<
-    UseMutationOptions<void, AxiosError, DeleteTodoDTO, typeof todosKeys.all>,
-    'mutationKey' | 'mutationFn'
-  >,
-) => {
+type DeleteTodoOptions = {
+  config?: MutationConfig<typeof deleteTodo>
+}
+export const useDeleteTodoMutation = (options?: DeleteTodoOptions) => {
   return useMutation({
     mutationFn: deleteTodo,
     mutationKey: todosKeys.all,
@@ -75,26 +52,25 @@ export const useDeleteTodoMutation = (
   })
 }
 
-export const useTodoQuery = <TData = Todo>(
+type FetchTodoFnType = typeof fetchTodo
+type FetchTodoOptions = {
+  config?: QueryConfig<FetchTodoFnType>
+}
+export const useTodoQuery = (
   { todoId }: FetchTodoDTO,
-  options?: Omit<
-    UseQueryOptions<Todo, AxiosError, TData>,
-    'queryKey' | 'queryFn'
-  >,
+  options?: FetchTodoOptions,
 ) => {
-  return useQuery({
+  return useQuery<ExtractFnReturnType<FetchTodoFnType>>({
     queryKey: [...todosKeys.each, todoId],
     queryFn: async () => fetchTodo({ todoId }),
     ...options,
   })
 }
 
-export const useUpdateTodoMutation = (
-  options?: Omit<
-    UseMutationOptions<void, AxiosError, UpdateTodoDTO>,
-    'mutationKey' | 'mutationFn'
-  >,
-) => {
+type UpdateTodoOptions = {
+  config?: MutationConfig<typeof updateTodo>
+}
+export const useUpdateTodoMutation = (options?: UpdateTodoOptions) => {
   return useMutation({
     mutationFn: updateTodo,
     mutationKey: todosKeys.each,
@@ -102,11 +78,11 @@ export const useUpdateTodoMutation = (
   })
 }
 
+type StoreImageForTodoOptions = {
+  config?: MutationConfig<typeof storeImageForTodo>
+}
 export const useStoreImageForTodoMutation = (
-  options?: Omit<
-    UseMutationOptions<void, AxiosError, StoreImageForTodoDTO>,
-    'mutationKey' | 'mutationFn'
-  >,
+  options?: StoreImageForTodoOptions,
 ) => {
   return useMutation({
     mutationFn: storeImageForTodo,
@@ -115,11 +91,11 @@ export const useStoreImageForTodoMutation = (
   })
 }
 
+type UpdateImageForTodoOptions = {
+  config?: MutationConfig<typeof updateImageForTodo>
+}
 export const useUpdateImageForTodoMutation = (
-  options?: Omit<
-    UseMutationOptions<void, AxiosError, UpdateImageForTodoDTO>,
-    'mutationKey' | 'mutationFn'
-  >,
+  options?: UpdateImageForTodoOptions,
 ) => {
   return useMutation({
     mutationFn: updateImageForTodo,
@@ -128,11 +104,11 @@ export const useUpdateImageForTodoMutation = (
   })
 }
 
+type DeleteImageForTodoOptions = {
+  config?: MutationConfig<typeof deleteImageForTodo>
+}
 export const useDeleteImageForTodoMutation = (
-  options?: Omit<
-    UseMutationOptions<void, AxiosError, DeleteImageForTodDTO>,
-    'mutationKey' | 'mutationFn'
-  >,
+  options?: DeleteImageForTodoOptions,
 ) => {
   return useMutation({
     mutationFn: deleteImageForTodo,
@@ -141,11 +117,11 @@ export const useDeleteImageForTodoMutation = (
   })
 }
 
+type StorePDFForTodoOptions = {
+  config?: MutationConfig<typeof storePDFForTodo>
+}
 export const useStorePDFForTodoMutation = (
-  options?: Omit<
-    UseMutationOptions<void, AxiosError, StorePDFForTodoDTO>,
-    'mutationKey' | 'mutationFn'
-  >,
+  options?: StorePDFForTodoOptions,
 ) => {
   return useMutation({
     mutationFn: storePDFForTodo,
